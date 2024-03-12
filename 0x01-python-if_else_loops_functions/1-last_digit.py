cf279ec07202cb1d7b4fd08febb3e6b10b98a565
Yermehyaw <yermehyaw@gmail.com>
2024-03-12 14:18:34 -0700

    Print the last digit of a rand no

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-#Check if the no is megative because negatives affect modulo arithmetic
+# Check if the no is megative because negatives affect modulo arithmetic
 if number < 0:
     no = number * -1
     sign = -1
 else:
     no = number
     sign = +1
-#Print strings according to value of the last number in the random no
+# Print strings according to value of the last number in the random no
 lt_no = (no % 10) * sign
 if lt_no > 5:
-    print(f"Last digit of {number} is {lt_no} and is greater than 5 and not 0")
+    print(f"Last digit of {number} is {lt_no} and is greater than 5")
 elif lt_no == 0:
     print(f"Last digit of {number} is {lt_no} and is 0")
 elif lt_no < 6 and not 0:

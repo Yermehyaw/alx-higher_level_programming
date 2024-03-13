50b7925047744f0ff7c62cde8e5b1338a661768e
Yermehyaw <yermehyaw@gmail.com>
2024-03-12 17:29:57 -0700

    Print all two digits from 0 to 99[fix-0]

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for tens in range(10):  # Tenth value in a 2 digit no from 0 to 9
-    for units in range(10):  # Unit values
-        if tens == 9 and units == 9:  # Print last iteration without a sep
-            print("{value}".format(value=str(tens) + str(units)))
-        else:  # Print all other iterations with a seperator
-            print("{value}".format(value=(str(tens) + str(units))), end=", ")
+for num in range(100):
+    if num == 99:  # Print last iteration without a sep
+        print("{value:02}".format(value=num))
+    else:  # Print all other iterations with a seperator
+        print("{value:02}".format(value=num), end=", ")

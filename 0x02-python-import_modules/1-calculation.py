838f4148718316d1e64ff638e6e75d31715539fb
Yermehyaw <yermehyaw@gmail.com>
2024-03-19 08:50:27 -0700

    Import a module and use its calculator functions on 2 no

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import calculator_1 as calc
+# import calculator_1 as calc
+from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
     a = 10
     b = 5
-    print("{a} + {b} = {ans}".format(a=a, b=b, ans=calc.add(a, b)))
-    print("{a} - {b} = {ans}".format(a=a, b=b, ans=calc.sub(a, b)))
-    print("{a} * {b} = {ans}".format(a=a, b=b, ans=calc.mul(a, b)))
-    print("{a} / {b} = {ans}".format(a=a, b=b, ans=calc.div(a, b)))
+    print("{a} + {b} = {ans}".format(a=a, b=b, ans=add(a, b)))
+    print("{a} - {b} = {ans}".format(a=a, b=b, ans=sub(a, b)))
+    print("{a} * {b} = {ans}".format(a=a, b=b, ans=mul(a, b)))
+    print("{a} / {b} = {ans}".format(a=a, b=b, ans=div(a, b)))

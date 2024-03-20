54296b6a3ea5fec06456a9f1c0c5de56aee329a0
Yermehyaw <yermehyaw@gmail.com>
2024-03-20 11:51:44 -0700

    Print all integers of a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_list_integer():
+    my_list = [1, 2]
+    i = 0
+    while i < len(my_list):
+        print("{}".format(my_list[i]))
+        i += 1
+
+print_list_integer()

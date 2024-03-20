ac311fcc2a70276e4092cab0f029dd1d3add0767
Yermehyaw <yermehyaw@gmail.com>
2024-03-20 14:14:40 -0700

    Relace an element of a list at a specific position[fix-2]

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    new_list = my_list
     if idx < 0:
         return None
     if idx >= 0:
         if idx >= len(my_list):
             return None
         my_list[idx] = element
-        return (new_list)
+        return (my_list)

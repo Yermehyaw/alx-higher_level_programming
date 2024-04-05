06baf7132456693029446655924d0f6f7ef3eff3
Yermehyaw <yermehyaw@gmail.com>
2024-04-05 12:54:06 -0700

    A class Square with a private attr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Modules impoted: None
+"""
+
+
+class Square:
+    """A 2d shape with four equal sides
+
+    Attributes:
+    __size (int): length of the square
+
+    """
+
+    def __init__(self, size):
+        """Defines instance attributes of the class Square
+
+        Args:
+        size (int): User specified size of square
+
+        """
+        self.__size = size

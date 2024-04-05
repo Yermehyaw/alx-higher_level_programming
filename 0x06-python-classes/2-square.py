c8daf30c56468442a858b9a2e5040635a841bf75
Yermehyaw <yermehyaw@gmail.com>
2024-04-05 14:33:27 -0700

    A clas Square that defines its attribute

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -20,7 +20,7 @@ class Square():
         size (int): User defined length of Sqare instance
 
         """
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")

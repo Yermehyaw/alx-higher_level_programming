4925263026aa1655cb70d90742bb85529b32be98
Yermehyaw <yermehyaw@gmail.com>
2024-04-10 23:45:46 +0100

    Exception pront a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,8 +2,9 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        for x in my_list:
-            print(my_list, end="")
+        i = 0
+        while (my_list[x]):
+            print(my_list[i], end="")
             print("")
             x -= 1
     except Exception:

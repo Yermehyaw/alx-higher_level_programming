9dace59c0c5c70067e005f742092b1f746c49be7
Yermehyaw <yermehyaw@gmail.com>
2024-04-12 21:03:39 +0100

    Fix: Indentation err

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,19 +6,19 @@ Module Imported: None
 
 
 def add_integer(a, b=98):
-     """Adds two integers
+    """Adds two integers
 
-     Args:
-     a (int): First no
-     b (int): Second no
+    Args:
+    a (int): First no
+    b (int): Second no
 
-     Returns:
-     Sum (int) of both integers
+    Returns:
+    Sum (int) of both integers
 
-     """
+    """
 
-     if not isinstance(a, (int, float)):
-         raise TypeError ("a must be an integer")
-     elif not isinstance(b, (int, float)):
-          raise TypeError("b must be an integer")
-     return (int(a) + int(b))
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))

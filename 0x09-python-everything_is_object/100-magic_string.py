52c37188423a456ddaf2546f3cc8377659d72d82
Yermehyaw <yermehyaw@gmail.com>
2024-04-14 02:11:32 +0100

    Return  a string “BestSchool” n number of iterations in a loop

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(count=[0]):
     count[0] += 1
-    return "Best School, " * (count[0] - 1) + "Best School"
+    return "BestSchool, " * (count[0] - 1) + "BestSchool"

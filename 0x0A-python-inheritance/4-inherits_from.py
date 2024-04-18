96df13b1438bc39554a32dc81e45714b9face352
Yermehyaw <yermehyaw@gmail.com>
2024-04-18 13:01:07 +0100

    Check if directly or indirectly inheritef

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+Imporred Module: None
+"""
+
+
+def inherits_from(obj, a_class):
+    """Returns a bool if an object is a direct or ibdirect inheritance of
+    a class
+
+    Args:
+    obj (unknown): Object name
+    a_class (unknown): ckass name
+
+    Returns:
+    True or False
+
+    """
+
+    return issubclass(obj, a_class)

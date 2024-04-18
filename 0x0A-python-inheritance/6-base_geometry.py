c0cff6acb8bb8890a6fa827286bc91a0dd8b1512
Yermehyaw <yermehyaw@gmail.com>
2024-04-18 13:19:24 +0100

    Improve BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Imported module: None
+"""
+
+
+class BaseGeometry:
+    """An empty parent class
+
+    Attributes:
+    None
+
+    """
+
+    def area(self):
+        """Raises an exception
+
+        Args:
+        None
+
+        Returns:
+        None
+
+        """
+
+        raise Exception("area() is not implemented")

ba619050cc206162b92b42b379f04f6bb8bc7f71
Yermehyaw <yermehyaw@gmail.com>
2024-04-18 12:21:00 +0100

    Check same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Imported Module: None
+"""
+
+def is_same_class(obj, a_class):
+    """ Returns a bool if an obhect is exactly tge sane with a specified class
+
+    Args:
+    obj (Unknown): Object name
+    a_class (Unknown): class name
+
+    Returns:
+    True or False
+
+    """
+
+    if type(obj) == type(a_class):
+        return True
+    else:
+        return False

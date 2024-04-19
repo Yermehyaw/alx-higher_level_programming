17214e56f9ec14b1b51c2d7ac1d905d3b308ab17
Yermehyaw <yermehyaw@gmail.com>
2024-04-19 17:51:51 +0100

    json serialization

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import json
+"""
+Imported module: json
+consists several methods for serializing and deserializing python data objects
+like ints, dicts, bools etc
+"""
+
+
+def to_json_string(my_obj):
+    """Returns the json rep of a python data object
+
+    Args:
+    my_obj (unknown): an object
+
+    Returns:
+    json string
+
+    """
+
+    return json.dumps(my_obj)

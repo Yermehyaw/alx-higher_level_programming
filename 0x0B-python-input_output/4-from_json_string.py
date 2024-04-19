dc10de1ca35eab767288c6b38bceab857289bb36
Yermehyaw <yermehyaw@gmail.com>
2024-04-19 18:02:06 +0100

    json deserialization

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import json
+"""Imported module: json
+consists several methods for serializing and deserializing python data objects
+like ints, dicts, bools etc
+"""
+
+
+def from_json_string(my_str):
+    """Returns the python data object of a json rep
+
+    Args:7
+    my_obj (unknown): an object
+
+    Returns:
+    python data object
+
+    """
+
+    return json.load(my_str)

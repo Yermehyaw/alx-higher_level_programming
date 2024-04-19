8aa311c6728bcd50733d2f1119defb93570e4968
Yermehyaw <yermehyaw@gmail.com>
2024-04-20 00:49:29 +0100

    Create obj from json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Imported module: json
+
+consists several methods for serializing and deserializing python data objects
+like ints, dicts, bools etc
+
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an object from a json file
+
+    Args:
+    filename (str): name of file
+
+    Returns:
+    A python data object
+
+    """
+
+    json_obj = json.load(filename)  # json_obj: obj made frm json
+    return json_obj

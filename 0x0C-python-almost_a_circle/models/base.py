4b146fc32ffeacfe7fc458ef4750279af08af741
Yermehyaw <yermehyaw@gmail.com>
2024-04-23 00:46:25 +0100

    Base class definition

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
 
 """
-Imported module(s):
+Imported module(s): None
 """
 
 
 class Base:
-    pass
+    """Base clasd for all incoming subclasses. A 2d shape
+
+    Attributes:
+    __nb_objects(int): no pf obects made from tgis class
+
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Object constructor/initializer
+
+        Arguments:
+        id (int): id no of the object
+
+        """
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            id = __nb_objects

ac81acd9f69d98db222544183febf2a7ae125151
Yermehyaw <yermehyaw@gmail.com>
2024-04-25 01:43:39 +0100

    Add documentation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,43 +20,85 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Object initializer
+
+        Args:
+        width (int)
+        height (int)
+        x(int)
+        y (int)
+        id (int): identity no - automatically inherited from parent class
+
+        """
+        super().__init__(id)
         self.set_width(width)
         self.set_height(height)
         self.set_x(x)
         self.set_y(y)
-        super().__init__(id)
 
     def get_width(self):
+        """Returns width value
+        """
         return self.__width
 
     def set_width(self, val):
+        """Inits the value of width
+
+        Args:
+        val (int): value to be set
+
+        """
         if isinstance(val, int):
             self.__width = val
         else:
             raise TypeError("Invalid number")
 
-    def get_height(self):
+    def get__height(self):
+        """Returns height value
+        """
         return self.__height
 
     def set_height(self, val):
+        """Inits the value of height
+
+        Args:
+        val (int): value to be set
+
+        """
         if isinstance(val, int):
             self.__height = val
         else:
             raise TypeError("Invalid number")
 
     def get_x(self):
+        """Returns x value
+        """
         return self.__x
 
     def set_x(self, val):
+        """Inits the value of x
+
+        Args:
+        val (int): value to be set
+
+        """
         if isinstance(val, int):
             self.__x = val
         else:
             raise TypeError("Invalid number")
 
     def get_y(self):
+        """Returns y  value
+        """
         return self.__y
 
     def set_y(self, val):
+        """Inits the value of y
+
+        Args:
+        val (int): value to be set
+
+        """
         if isinstance(val, int):
             self.__y = val
         else:

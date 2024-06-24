fd8a9b6e42192514a5a928c7727974f9556ed7e2
Yermehyaw <yermehyaw@gmail.com>
2024-06-23 23:52:52 -0700

    Create a Rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * 0-rectangle - creates a rectangle class
+ */
+class Rectangle {
+  /**
+   * Rectangle - defines a 2d rectangle shape
+   *
+   * Attributes (properties):
+   * null
+   */
+  constructor () {
+    return this;
+  }
+}

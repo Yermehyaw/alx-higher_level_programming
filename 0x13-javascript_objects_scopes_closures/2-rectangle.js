226d4bf500030f2d94564569fe5bc0acd69eca2f
Yermehyaw <yermehyaw@gmail.com>
2024-06-24 00:08:11 -0700

    Add a print() method to Rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+/**
+ * 0-rectangle - creates a rectangle class
+ */
+class Rectangle {
+  /**
+   * Rectangle - defines a 2d rectangle shape
+   *
+   * Attributes (properties):
+   *   width(int): width of rectangle
+   *   height(int): height of rectangle
+   */
+  constructor (w, h) {
+    /**
+      * Constructor method for rectangle class
+      *
+      * Args:
+      *   w(int): width of rectangle
+      *   h(int): height of rectangle
+      */
+    if (width <= 0 || height <= 0)
+      return this;
+    const width = w;
+    const height = h;
+    return this;
+  }
+}

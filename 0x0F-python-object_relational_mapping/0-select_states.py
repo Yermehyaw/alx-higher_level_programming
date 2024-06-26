96aece14f91d09d35ee7571ff927896ccd3567db
Yermehyaw <yermehyaw@gmail.com>
2024-06-26 00:42:29 -0700

    Query a db in for name col in asc order

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+"""
+Modules Imported: sys, MySQLdb
+
+sys
+Library for executing shell commands from a pyscript
+
+MySQLdb
+Managing MySQL databases from apyscript
+"""
+import sys
+import MySQLdb
+
+
+if (__name__ == '__main__'):
+    try:
+        conn_db = MySQLdb.connect(host=localhost, user=sys.argv[1],
+                                  passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    except ():
+        return (1)
+    cur_1 = conn_db.cursor()
+    name_row_value = cur_1.execute(f"SELECT * FROM {sys.argv[3]} ORDER BY\
+states.id ASC")
+    print(name_row_value)
+    return (0)

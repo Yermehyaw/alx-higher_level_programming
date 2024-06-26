7d542187330a904deb160ad816db21d805f93e45
Yermehyaw <yermehyaw@gmail.com>
2024-06-26 08:43:39 -0700

    Fix documen err

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,7 @@
 """
 Modules Imported: sys, MySQLdb
 sys: Library for executing shell commands from a pyscript
-MySQLdb: Managing MySQL databases from apyscript
+MySQLdb: Managing MySQL databases from a pyscript
 """
 import sys
 import MySQLdb
@@ -11,12 +11,12 @@ import MySQLdb
 
 if __name__ == '__main__':
     try:
-        conn_db = MySQLdb.connect(host=localhost, user=sys.argv[1], port=3306
-                                  passwd=sys.argv[2], db=sys.argv[3])
+        conn_db = MySQLdb.connect(host=localhost, user=sys.argv[1], port=3306, passwd=sys.argv[2], db=sys.argv[3])
     except ():
-        return (1)
+        print('')
+#        return
     cur_1 = conn_db.cursor()
     name_row_value = cur_1.execute(f"SELECT * FROM {sys.argv[3]} ORDER BY\
 states.id ASC")
     print(name_row_value)
-    return (0)
+#    return (0)

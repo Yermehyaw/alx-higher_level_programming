cad6822dee614e303e41a83bdbccfac83adcfb13
Yermehyaw <yermehyaw@gmail.com>
2024-06-26 13:33:47 -0700

    Fix sql query - 2

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,13 +10,26 @@ import MySQLdb
 
 
 if __name__ == '__main__':
+    db_name = sys.argv[3]
+    user_passwd = sys.argv[2]
+    user_name = sys.argv[1]
     try:
-        conn_db = MySQLdb.connect(host=localhost, user=sys.argv[1], port=3306, passwd=sys.argv[2], db=sys.argv[3])
-    except ():
-        print('')
-#        return
+        conn_db = MySQLdb.connect(
+            host='localhost',
+            user=user_name,
+            port=3306,
+            passwd=user_passwd,
+            db=db_name
+        )
+    except Exception:
+        print('Couldnt connect to db')
+#        return None
     cur_1 = conn_db.cursor()
-    name_row_value = cur_1.execute(f"SELECT * FROM {sys.argv[3]} ORDER BY\
-id ASC")
-    print(name_row_value)
-#    return (0)
+    name_row_value = cur_1.execute(f"SELECT * FROM states ORDER BY\
+ id ASC")
+    table = cur_1.fetchall()
+    for row in table:
+        print(row)
+
+    cur_1.close()
+    conn_db.close()

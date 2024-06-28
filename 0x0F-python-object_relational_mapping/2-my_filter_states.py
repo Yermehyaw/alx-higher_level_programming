95dd5e836cc110d83701c5b9d9975a0aef6dbcbd
Yermehyaw <yermehyaw@gmail.com>
2024-06-28 15:41:49 +0100

    Display all values in table which matches a passd arg

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Modules Imported: sys, MySQLdb
+sys: Library for executing shell commands from a pyscript
+MySQLdb: Managing MySQL databases from a pyscript
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    searched = sys.argv[4]  # Entry to find and print fron db - Optional
+    db_name = sys.argv[3]
+    user_passwd = sys.argv[2]
+    user_name = sys.argv[1]]
+    try:
+        conn = MySQLdb.connect(
+                host='localhost',
+                user=user_name,
+                passwd=user_passwd,
+                db=db_name
+        )
+    except (Exception):
+        print('Couldnt connect to the database')
+
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM states WHERE name = {} ORDER \
+            by id ASC'.format(searched))
+
+    table = cur.fetchall()
+    for row in table:
+        print(row)
+
+    cur.close()
+    conn.close()

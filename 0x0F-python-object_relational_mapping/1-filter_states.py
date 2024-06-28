2395f5a2a9363dd07d5a5f2634b82207b9dbe7fd
Yermehyaw <yermehyaw@gmail.com>
2024-06-28 15:23:45 +0100

    List all states in specified DB starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Modules Imported: sys, MySQLdb
+sys: Library for executing shell commands from a pyscript
+MySQLdb: Managing MySQL databases from a pyscript
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db_name = sys.argv[3]
+    user_passwd = sys.argv[2]
+    user_name = sys.argv[1]
+    try:
+        conn_db = MySQLdb.connect(
+            host='localhost',
+            user=user_name,
+            port=3306,
+            passwd=user_passwd,
+            db=db_name
+        )
+    except Exception:
+        print('Couldnt connect to db')
+
+    cur_1 = conn_db.cursor()
+    name_row_value = cur_1.execute(f"SELECT * FROM states WHERE name LIKE 'N%' ORDER BY\
+ id ASC")
+
+    table = cur_1.fetchall()
+    for row in table:
+        print(row)
+
+    cur_1.close()
+    conn_db.close()

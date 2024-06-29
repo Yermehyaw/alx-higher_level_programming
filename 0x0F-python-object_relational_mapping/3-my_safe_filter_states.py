209a0a9d90ca5c2ed580ac90911ef9c6ba666e05
Yermehyaw <yermehyaw@gmail.com>
2024-06-29 10:42:48 -0700

    Improve SQL injection defence

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,22 +10,25 @@ import MySQLdb
 
 
 if __name__ == '__main__':
+    # Preventing SQL injection from an optional argument
     try:
         searched = sys.argv[4]  # Entry to be received fron shell - Optional
-        split_searched = searched.split()  # Check if more than one name is entered
-        if split_searched[1] != None:
+        if not isinstance(searched, str):  # must be a string
             raise ValueError
+        split_searched = searched.split()  # Check if more than one word is entered
+        err = split_searched[1]  # Should raise a IndexError exception
         # Check if a semicolon is in the arg to prevent SQL injection
         if searched != None:
             for char in searched:
-                if char = ';'
-                raise ValueError
-    except (ValueError):
+                if char == ';':
+                    raise ValueError
+    except (AttributeError, ValueError, IndexError):
         print('Invalid entry requested')
+        return
     # try/except isnt used to validate the following inputs
     db_name = sys.argv[3]
     user_passwd = sys.argv[2]
-    user_name = sys.argv[1]]
+    user_name = sys.argv[1]
     # Catch any invalid database parameters passed 
     try:
         conn = MySQLdb.connect(
@@ -38,7 +41,11 @@ if __name__ == '__main__':
         print('Couldnt connect to the database')
 
     cur = conn.cursor()
-    cur.execute('SELECT * FROM states WHERE name = {} ORDER by id ASC'.format(searched))
+    query = "SELECT * FROM states \
+            WHERE BINARY name = '{}' \
+            ORDER by id ASC".format(searched)
+
+    cur.execute(query)
 
     table = cur.fetchall()
     for row in table:

8236c930236fc39634a882ff4844a4ad2439ce6e
Yermehyaw <yermehyaw@gmail.com>
2024-07-06 17:11:20 +0100

    Template file

---
+++ 0x11-python-network_1/rawfile.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Modules Imported: urllib.request
+
+urllib.request: Fetch URL resources from servers
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+
+    """
+    """

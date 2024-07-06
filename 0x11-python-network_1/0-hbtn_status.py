78f6da4679bc6dc37b2fb08ae2ddf964dd8be232
Yermehyaw <yermehyaw@gmail.com>
2024-07-06 17:11:01 +0100

    Fetch 'https://alx-intranet.hbtn.io/status' with urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Modules Imported: urllib.request
+
+urllib.request: Fetch URL resources from servers
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+
+    """
+    Fetch https://alx-intranet.hbtn.io/status webpage
+    """
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:\n\t-{html.type}\n\t-{html.content}\n
+        \t-{html.utf8}")

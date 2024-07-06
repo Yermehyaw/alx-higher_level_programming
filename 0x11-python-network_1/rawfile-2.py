4c0d7b89b08434e77cebb966efc2248d65625051
Yermehyaw <yermehyaw@gmail.com>
2024-07-06 22:15:43 +0100

    Update template file

---
+++ 0x11-python-network_1/rawfile-2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Modules/Packages Imported: requests
+
+requests: Various modules and methods for fetching URL resources from servers
+sys: Use shell commands and args in pyscript
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    """
+    """
+    if len(sys.argv) < :
+        return
+

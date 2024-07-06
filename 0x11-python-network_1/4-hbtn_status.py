fa19323d16bc3ccd867cc686f09eb7718bb5a2c1
Yermehyaw <yermehyaw@gmail.com>
2024-07-06 21:27:27 +0100

    Fetch resource/page from a URL

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Modules/Packages Imported: requests
+
+requests: Various modules and methods for fetching URL resources from servers
+"""
+import requests
+
+
+if __name__ == "__main__":
+
+    """
+    Fetches https://alx-intranet.hbtn.io/status
+    """
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    page = req.text
+    print(f"Body response:\n\t-{page[type]}\n\t-{page[content]}")

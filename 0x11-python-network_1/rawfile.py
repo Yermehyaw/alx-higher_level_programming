4a226ac84b934f52fcc4be90e3871d3e9eb75156
Yermehyaw <yermehyaw@gmail.com>
2024-07-06 17:13:23 +0100

    Template file

--- 0x11-python-network_1/rawfile.py
+++ 0x11-python-network_1/rawfile.py
@@ -4,8 +4,10 @@
 Modules Imported: urllib.request
 
 urllib.request: Fetch URL resources from servers
+sys: Use shell commands and args in pyscript
 """
 import urllib.request
+import sys
 
 
 if __name__ == "__main__":

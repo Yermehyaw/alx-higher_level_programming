abb59c7926032045431746a257a16866dbe0cc48
Yermehyaw <yermehyaw@gmail.com>
2024-07-06 04:53:39 -0700

    Fix shebang

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 #  takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -s -X "GET" -o /dev/stdout -w "%{http_code}" "$1" | grep -w "200"

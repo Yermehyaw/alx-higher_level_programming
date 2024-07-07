cca16461ab57a66b6e55eff72116920be8d43422
Yermehyaw <yermehyaw@gmail.com>
2024-07-07 14:53:11 -0700

    POST for JSON validation

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # JSON file POST request to a URL and displays the body of the response
-curl -X "P0ST" -H "Content-Type: application/json" --data-binary "@./$2" "$1"
+curl -X "P0ST" -H "Content-Type: application/octet-stream" --data-binary "@./$2" "$1"  # octet-stream posts ge fike as raw binary for a JSON valodation resposonse by the server

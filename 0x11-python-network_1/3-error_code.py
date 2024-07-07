c0aa9936e1c5320d2911f44d65509679dbc383c5
Yermehyaw <yermehyaw@gmail.com>
2024-07-06 19:21:20 -0700

    Change code preferences

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 """
-Modules/Packages Imported: urllib, sys
+Modules/Packages Imported: urllib.request, sys
 
-urllib: Various modules and methods for fetching URL resources from servers
+urllib.request: Various modules and methods for fetching URL resources
+ from servers
 sys: Use shell commands and args in pyscript
 """
-import urllib
+import urllib.request
 import sys
 
 
@@ -16,13 +17,13 @@ if __name__ == "__main__":
     Takes in a URL, sends a request to the URL and displays the body
     of the response
     """
-    if len(sys.argv) > 2:
-        return
+    if len(sys.argv) < 2:
+        exit(1)
     url = sys.argv[1]
     req = urllib.request.Request(url)  # URL Request not sent yet only prepared
     try:
         with urllib.request.urlopen(req) as response:  # Request is made here
             page = response.read()
             print(page)
-    except (urllib.error.HTTPError) as e:  # e has a unique attr: e.code
-        print(e.code)
+    except (urllib.error.HTTPError) as err_resp:  # e has a unique attr: e.code
+        print('Error code: {}'.format(err_resp.code))

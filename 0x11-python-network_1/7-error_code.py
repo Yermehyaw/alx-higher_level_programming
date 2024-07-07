2db52f35c74de24a8112819bd385111ba18272e3
Yermehyaw <yermehyaw@gmail.com>
2024-07-07 15:21:15 -0700

    Fix eception handling

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -19,9 +19,21 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         exit(1)
     url = sys.argv[1]
-    page = requests.get(url)
-    stat_code = page.status_code
-    if (stat_code < 400):
-        print(page.text)
-    else:
-        print("Eror code:", stat_code)
+    try:  # In case an exception is raised as url resource is fetched
+        page = requests.get(url)
+        stat_code = page.status_code
+        if (stat_code < 400):
+            print(page.text)
+        else:
+            print("Eror code:", stat_code)  # Any other 'bad' status code
+    except requests.exceptions.RequestException as resp_err:
+        print('Error: ', resp_err)  # No status_code attribute in this exception
+    except requests.exceptions.HTTPError as http_err:
+        print('Error code:', http_err.response.status_code)
+    except requests.exceptions.ConnectionError as conn_err:
+        print('Error: ', conn_err)  # Same here
+    except requests.exceptions.Timeout as time_err:
+        print('Error: ', time_err)  # and here too
+    except Exception:  # Any other one which only the Lord knows ....
+        print("An unknown error ocurred")
+

857768015a58fcb31fe914eb255ffeda943b5d16
Yermehyaw <yermehyaw@gmail.com>
2024-07-06 18:29:57 -0700

    Fix syntax

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # JSON file POST request to a URL and displays the body of the response
-curl -X "P0ST" -H "Content-Type: application/json" --data-binary "./$2" "$1"
+curl -X "P0ST" -H "Content-Type: application/json" --data-binary "@./$2" "$1"

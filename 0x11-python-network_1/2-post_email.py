755fcb2e9e74ce4c23a02a126c245414e7596718
Yermehyaw <yermehyaw@gmail.com>
2024-07-06 18:59:59 -0700

    Fix syntax

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
     Takes in a URL and an email, sends a POST request to the passed
     URL with the email as a parameter, and displays the body of the response
     """
-    if sys.argv > 3:
-        return
+    if len( sys.argv) < 3:
+        exit(1)
     url = sys.argv[1]
     email = sys.argv[2]
     params = {'email': email}  # data to POST in URL

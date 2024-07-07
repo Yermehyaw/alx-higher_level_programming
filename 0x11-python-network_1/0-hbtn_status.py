929da35741a260d0a98a12156dc5918fffa2ecfc
Yermehyaw <yermehyaw@gmail.com>
2024-07-06 17:18:51 -0700

    Fix pycodestyle errs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,8 +12,10 @@ if __name__ == "__main__":
 
     """
     Fetch https://alx-intranet.hbtn.io/status webpage
+
     """
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
+    as response:
         html = response.read()
-        print("Body response:\n\t-{html.type}\n\t-{html.content}\n
-        \t-{html.utf8}")
+        print("Body response:\n\t-{html.type}\n\t-{html.content}\n\
+\t-{html.utf8}")

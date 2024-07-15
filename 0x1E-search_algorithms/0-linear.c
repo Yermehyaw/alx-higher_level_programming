176bf9732fbb544413ccd20ec7b8a475958456bb
Yermehyaw <yermehyaw@gmail.com>
2024-07-15 14:48:39 -0700

    Fix code structure

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -20,10 +20,9 @@ int linear_search(int *array, size_t size, int value)
 		return (0);  /* Index 0 */
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
-		else
-			printf("Value checked array[%d] = [%d]\n", i, array[i]);
 	}
 	return (-1);
 }

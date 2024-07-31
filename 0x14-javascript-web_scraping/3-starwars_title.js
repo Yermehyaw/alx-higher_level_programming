3b133c12dd565ea9f9a544d0c158ada6ceb3935d
Yermehyaw <yermehyaw@gmail.com>
2024-07-31 06:05:38 +0100

    Fix code struct and properties

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -8,8 +8,9 @@ const movieId = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/$(movieId)';
 request(url, function (err, response, body) {
   if (err) {
-    console.log('Connection failed');
+    return console.log('Connection failed');
   } else {
-    console.log(body.parse['title']);
+    const movieTitle = JSON.parse(body);
+    console.log(movieTitle['title']);
   }
 });

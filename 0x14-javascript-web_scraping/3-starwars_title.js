6f52155b9c2ff9dd1e6fddef921eaf5cc424d606
Yermehyaw <yermehyaw@gmail.com>
2024-07-31 05:57:20 +0100

    Fix syntax err

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,7 +7,7 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/$(movieId)';
 request(url, function (err, response, body) {
-  if (err) || response.statusCode >= 400
+  if (err)
     console.log('Connection failed');
   } else {
     console.log(body.parse['title']);

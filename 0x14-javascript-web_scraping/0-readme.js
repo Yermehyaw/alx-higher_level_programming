a0aec3d0863193bbb15f977d7bfd1bcbfb13d7de
Yermehyaw <yermehyaw@gmail.com>
2024-07-31 04:51:24 +0100

    Print contents of a file to console

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,14 @@
 /**
  * Print contents of a file to console
  */
-let fs = require("fs");
+const fs = require('fs').promises;
 
 const fileName = process.argv[2];  /*[2] is the first command arg in shell*/
+fs.readFile(fileName, 'utf-8', (err, data) => {
+  if (err) 
+    console.log(err, message);
+  } else {
+    conole.log(data);
+  }
+});
+

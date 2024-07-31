f6376c1f6bfe07d51a0aa54d9ea8531868e32e0a
Yermehyaw <yermehyaw@gmail.com>
2024-07-31 05:02:27 +0100

    Write to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ * Print contents of a file to console
+ */
+const fs = require('fs');
+
+const fileName = process.argv[2];  /*[2] is the first command arg in shell*/
+const toWrite = process.argv[3];
+fs.writeFile(fileName, toWrite, 'utf-8', (err, data) => {
+  if (err)
+    console.log(err, message);
+});
+

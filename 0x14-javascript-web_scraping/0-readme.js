ccfbc386c1a9e373db1096e19f53149072d9880e
Yermehyaw <yermehyaw@gmail.com>
2024-07-31 05:15:55 +0100

    Remove undefined variable

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const fileName = process.argv[2];/* [2] is the first command arg in shell */
 fs.readFile(fileName, 'utf-8', (err, data) => {
   if (err) {
-    console.log(err, message);
+    console.log(err);
   } else {
     console.log(data);
   }

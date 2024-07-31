3d4e548292bbf761da8b8c5ca6d86326d2278757
Yermehyaw <yermehyaw@gmail.com>
2024-07-31 05:19:35 +0100

    Fix syntax err

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 const fileName = process.argv[2];/* [2] is the first command arg in shell */
 const toWrite = process.argv[3];
 fs.writeFile(fileName, toWrite, 'utf-8', (err, data) => {
-  if (err)
+  if (err) {
     console.log(err);
+  }
 });

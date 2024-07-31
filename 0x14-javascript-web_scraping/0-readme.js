86a206e365d122dc735abcfd4560b5cfb5fdc3cf
Yermehyaw <yermehyaw@gmail.com>
2024-07-31 04:55:18 +0100

    Remove .promises import on fs

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 /**
  * Print contents of a file to console
  */
-const fs = require('fs').promises;
+const fs = require('fs');
 
 const fileName = process.argv[2];  /*[2] is the first command arg in shell*/
 fs.readFile(fileName, 'utf-8', (err, data) => {

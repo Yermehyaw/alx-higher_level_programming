c5b38d5ea941e32384c95d2dad28d7493db40c77
Yermehyaw <yermehyaw@gmail.com>
2024-08-01 07:13:50 -0700

    Fix semistabdard style errs

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,22 +6,22 @@
 const request = require('request');
 
 const url = process.argv[2];
-characterURL = 'https://swapi-api.alx-tools.com/api/people/18/';
+const characterURL = 'https://swapi-api.alx-tools.com/api/people/18/';
+console.log(characterURL);
 request(url, function (error, response, body) {
   if (error || response.statusCode >= 400) {
     return console.log('Connection failed');
   }
   let noOfFeatures = 0;
   const jsonBody = JSON.parse(body);
-  const allFilms = jsonBody.results;  /* a list of dicts */
+  const allFilms = jsonBody.results; /* a list of dicts */
   for (const movieDetail of allFilms) {
-    const characters = movieDetail.characters /* a list */
+    const characters = movieDetail.characters; /* a list */
     for (const character of characters) {
-      if (character == characterURL) {
+      if (character === characterURL) {
         noOfFeatures += 1;
       }
     }
   }
-  return console.log(noOfFeatures)
+  return console.log(noOfFeatures);
 });
-

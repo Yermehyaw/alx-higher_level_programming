1ab86dcd41708fb3b9cf8a73509b73fa39f08821
Yermehyaw <yermehyaw@gmail.com>
2024-08-01 05:58:07 -0700

    Print titke of star wars movie from an API

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 /**
- * Print th title of a STAR Wars movie from an API
+ * Print the title of a STAR Wars movie from an API
  */
 const request = require('request');
 
 const movieId = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/$(movieId)';
-request(url, function (err, response, body) {
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+request.get(url, function (err, response, body) {
   if (err) {
     return console.log('Connection failed');
   } else {
     const movieTitle = JSON.parse(body);
-    console.log(movieTitle['title']);
+    console.log(movieTitle.title);
   }
 });

1246ded40cfeb74ad38092d4ad3a749e62301c35
Yermehyaw <yermehyaw@gmail.com>
2024-08-07 14:10:19 -0700

    Fix semistandard style er

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,6 +1,6 @@
 /**
  * Update the text color of the <header> element to red (#FF0000)
  */
-const header = document.querySelector("header");
+const header = document.querySelector('header');
 
-header.style.color = "#FF0000";
+header.style.color = '#FF0000';

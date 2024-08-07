f2153486af700c460259eeb532942ed9cbf331a4
Yermehyaw <yermehyaw@gmail.com>
2024-08-07 16:08:42 -0700

    Fix linter er

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -2,7 +2,7 @@
  * updates the text color of the <header> element to red
  *  when the user clicks on the tag DIV#red_header
  */
-const $ = window.$
+const $ = window.$;
 $(document).ready(function () {
   $('div#red_header').on('click', function () {
     $(this).css('color', 'red');

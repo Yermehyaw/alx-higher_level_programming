ba30f71889649a162f9c59c9b231860ba7cf7ab8
Yermehyaw <yermehyaw@gmail.com>
2024-08-07 20:10:05 -0700

    Update elem color with <style> in <head>

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+/**
+ * Update the color of an elem regardless of the post. of <style>
+ * in html file
+ */
+document.addEventListener('DOMContentLoaded', (event) => {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});

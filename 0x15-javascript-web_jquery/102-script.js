300fb5f2ca22d878df0041bf5e77362ce52da554
Yermehyaw <yermehyaw@gmail.com>
2024-08-11 01:03:58 +0100

    Add proxy

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -4,11 +4,11 @@
 const $ = window.$;
 $(document).ready(function () {
   const lang = $('input#language_code').val()
-  url = `https://www.fourtonfish.com/hellosalut/hello/?lang=$(lang)`;
-  $.get(url, function (response, status) {
-    $('input#btn_translate').on('click', function () {
+  url = 'https://api.cors.lol/url=' + `https://www.fourtonfish.com/hellosalut/hello/?lang=${lang}`;
+  $('input#btn_translate').on('click', function () {
+    $.get(url, function (response, status) {
       if (status === 'success')
-	$('div#hello').text(response.hello)
+        $('div#hello').text(response.hello)
     });
   });
 });

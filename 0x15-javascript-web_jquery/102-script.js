3eafade1daffc459a4b4d3bff3b685b0d6081996
Yermehyaw <yermehyaw@gmail.com>
2024-08-12 18:09:28 +0100

    Fix missing semicolon

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -3,7 +3,7 @@
  */
 const $ = window.$;
 $(document).ready(function () {
-  const lang = $('input#language_code').val()
+  const lang = $('input#language_code').val();
   const url = 'https://api.cors.lol/url=' + `https://www.fourtonfish.com/hellosalut/hello/?lang=${lang}`;
   $('input#btn_translate').on('click', function () {
     $.get(url, function (response, status) {

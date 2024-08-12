3a221e52e23ebd655b6c701036560178de805080
Yermehyaw <yermehyaw@gmail.com>
2024-08-12 18:58:53 +0100

    Dynamic 102.html

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,10 +1,16 @@
 /**
  * Translate the <header> based on user input
+ *
+ * NOTE: 'https://www.fourtonfish.com/hellosalut/hello/' has been permanently
+ * moved to 'https://hellosalut.stefanbohacek.dev/'
+ * Try https://www.fourtonfish.com/hellosalut/hello/lang=fr to see the broken
+ * webpage
  */
 const $ = window.$;
 $(document).ready(function () {
   const lang = $('input#language_code').val();
-  const url = 'https://api.cors.lol/url=' + `https://www.fourtonfish.com/hellosalut/hello/?lang=${lang}`;
+  const proxy = `https://api.cors.lol/url=`;
+  const url = `${proxy}https://hellosalut.stefanbohacek.dev/?lang=${lang}`;
   $('input#btn_translate').on('click', function () {
     $.get(url, function (response, status) {
       if (status === 'success')

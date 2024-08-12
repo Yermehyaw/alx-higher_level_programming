7af76ae3827f42204b669909cf89c86caa9542cb
Yermehyaw <yermehyaw@gmail.com>
2024-08-12 18:08:23 +0100

    Fix syntax errs

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -4,7 +4,7 @@
 const $ = window.$;
 $(document).ready(function () {
   const lang = $('input#language_code').val()
-  url = 'https://api.cors.lol/url=' + `https://www.fourtonfish.com/hellosalut/hello/?lang=${lang}`;
+  const url = 'https://api.cors.lol/url=' + `https://www.fourtonfish.com/hellosalut/hello/?lang=${lang}`;
   $('input#btn_translate').on('click', function () {
     $.get(url, function (response, status) {
       if (status === 'success')

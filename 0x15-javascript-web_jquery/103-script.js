6a755d006ec8c54a7898f9b37ac8f4bde87b7685
Yermehyaw <yermehyaw@gmail.com>
2024-08-13 08:45:12 +0100

    Dynamic enter key

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,28 @@
+/**
+ * Translate the <header> based on user input
+ *
+ * NOTE: 'https://www.fourtonfish.com/hellosalut/hello/' has been permanently
+ * moved to 'https://hellosalut.stefanbohacek.dev/'
+ * Try https://www.fourtonfish.com/hellosalut/hello/lang=fr to see the broken
+ * webpage
+ */
+const $ = window.$;
+$(document).ready(function () {
+  const proxy = `https://api.cors.lol/url=`;
+  $('input#btn_translate').on('click', function () {
+    const lang = $('input#language_code').val();
+    const url = `${proxy}https://hellosalut.stefanbohacek.dev/?lang=${lang}`;
+    $.get(url, function (response, status) {
+      if (status === 'success')
+        $('div#hello').text(response.hello)
+    });
+  $('input#btn_translate').keypress('click', function () {
+    const lang = $('input#language_code').val();
+    const url = `${proxy}https://hellosalut.stefanbohacek.dev/?lang=${lang}`;
+    $.get(url, function (response, status) {
+      if (status === 'success')
+        $('div#hello').text(response.hello)
+    });
+  });
+  });
+});

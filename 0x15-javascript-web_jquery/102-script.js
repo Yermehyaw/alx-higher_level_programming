e0ec2e31ddc59f92cbb4ee3f1dfbf30eb55ed9df
Yermehyaw <yermehyaw@gmail.com>
2024-08-13 08:46:31 +0100

    Fix code struct

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -8,10 +8,10 @@
  */
 const $ = window.$;
 $(document).ready(function () {
-  const lang = $('input#language_code').val();
   const proxy = `https://api.cors.lol/url=`;
-  const url = `${proxy}https://hellosalut.stefanbohacek.dev/?lang=${lang}`;
   $('input#btn_translate').on('click', function () {
+    const lang = $('input#language_code').val();
+    const url = `${proxy}https://hellosalut.stefanbohacek.dev/?lang=${lang}`;
     $.get(url, function (response, status) {
       if (status === 'success')
         $('div#hello').text(response.hello)

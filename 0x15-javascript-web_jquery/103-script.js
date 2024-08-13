061163eef74f118d6e81f75f2c560cb8223171d5
Yermehyaw <yermehyaw@gmail.com>
2024-08-13 09:38:13 +0100

    Add keypress event handler

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -8,21 +8,25 @@
  */
 const $ = window.$;
 $(document).ready(function () {
-  const proxy = `https://api.cors.lol/url=`;
+  const proxy = `https://corsproxy.io/?`;
   $('input#btn_translate').on('click', function () {
     const lang = $('input#language_code').val();
-    const url = `${proxy}https://hellosalut.stefanbohacek.dev/?lang=${lang}`;
-    $.get(url, function (response, status) {
-      if (status === 'success')
-        $('div#hello').text(response.hello)
-    });
-  $('input#btn_translate').keypress('click', function () {
-    const lang = $('input#language_code').val();
-    const url = `${proxy}https://hellosalut.stefanbohacek.dev/?lang=${lang}`;
+    const translationAPI = `https://hellosalut.stefanbohacek.dev/?lang=${lang}`;
+    const url = `${proxy}` + encodeURIComponent(translationAPI);
     $.get(url, function (response, status) {
       if (status === 'success')
         $('div#hello').text(response.hello)
     });
   });
+  $('input#language_code').on('keypress', function (event) {
+    if (event.which === 13) {
+      const lang = $('input#language_code').val();
+      const translationAPI = `https://hellosalut.stefanbohacek.dev/?lang=${lang}`;
+      const url = `${proxy}` + encodeURIComponent(translationAPI);
+      $.get(url, function (response, status) {
+        if (status === 'success')
+          $('div#hello').text(response.hello)
+      });
+    }
   });
 });
